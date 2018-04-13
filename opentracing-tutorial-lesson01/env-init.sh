ba02bf7fd64e5e19047e8cc301a72791e4a74338
Ben Hall <ben@benhall.me.uk>
2018-04-13 17:33:59 +0100

    Fixes #2 by moving to Ubuntu. Move env-init to ssh as that's required for Ubuntu

--- opentracing-tutorial-lesson01/env-init.sh
+++ opentracing-tutorial-lesson01/env-init.sh
@@ -1,4 +1,1 @@
-apt-get update \
-  && apt-get install -y git openjdk-8-jdk maven \
-  && git clone https://github.com/jpkrohling/opentracing-tutorial -b Katacoda \
-  && docker run -d -p5775:5775/udp -p6831:6831/udp -p6832:6832/udp -p5778:5778 -p16686:16686 -p14268:14268 -p9411:9411 jaegertracing/all-in-one:1.3
+ssh root@host01 "git clone https://github.com/jpkrohling/opentracing-tutorial -b Katacoda && docker run -d -p5775:5775/udp -p6831:6831/udp -p6832:6832/udp -p5778:5778 -p16686:16686 -p14268:14268 -p9411:9411 jaegertracing/all-in-one:1.3"
